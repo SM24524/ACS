8be11a6041cec335fef19904b21052a5bfd17c9b
SM24524 <56416969+SM24524@users.noreply.github.com>
2022-09-26 22:07:48 -0400

    Create ScrabbleDriver.java

---
+++ ScrabbleDriver.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class ScrabbleDriver{
+    public static void main(String args[]){
+        try{
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter the file name: ");
+            String fileName = s.nextLine();
+            TextFileAccessor scrab = new MaxScrabbleScore();
+            scrab.openFile(fileName);
+            scrab.processFile();
+            System.out.println(scrab.getReportStr());
+            s.close();
+        }catch(IOException ioex){
+            System.out.println(ioex);
+        }
+    }
+}
